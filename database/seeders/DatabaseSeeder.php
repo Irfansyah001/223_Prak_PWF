d92c139280c280c5ba2931aea785304db0cf3d1c
汉堡包 <syahirfan545@gmail.com>
2026-03-07 12:54:48 +0700

    feat: update DatabaseSeeder to use specific user data and include factories for products and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +18,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Irfansyah',
+            'email' => 'irfansyah@gmail.com',
+            'password' => bcrypt('password'),
         ]);
+
+        User::factory(10)->create();
+        Product::factory(20)->create();
+        Category::factory(10)->create();
     }
 }
